5e9bcab9dcc8ea81e3b990bbb56f6d31e1f9f619
Damian Attia-Barillas <145603410+da45da@users.noreply.github.com>
2024-11-27 21:22:06 -0500

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 100; i > 20; i++)
+            {
+                if (i % 1000000 == 0)
+                    Console.WriteLine(i.ToString());
+            }
+
+        }
+    }
+}
